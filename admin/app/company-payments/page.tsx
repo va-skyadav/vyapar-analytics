79a9ef5bc565abf94467afe79edc3569546bbef3
va-skyadav <vyaparanalytics@gmail.com>
2026-09-26 11:56:09 +0530

    feat: add dedicated company payments admin route

---
+++ admin/app/company-payments/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import {useEffect} from "react";
+
+export default function CompanyPayments(){
+  useEffect(()=>{ window.location.replace("/subscription?tab=payments"); },[]);
+  return <div className="adminLoading"><div className="card">Opening Company & Payments...</div></div>;
+}
